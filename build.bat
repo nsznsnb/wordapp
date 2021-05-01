81584ba3d7985f0349dcbfb97f6792f3abed4000
nsznsnb <nsznsnb@gmail.com>
2021-05-01 18:46:13 +0900

    jdbcのアップデートに関連して、修正を加えた

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
 @echo off
-rem ソースファイルのコンパイル
 javac -encoding UTF-8 -sourcepath src -d bin src\wordapp\gui\MainFrame.java
-rem java -cp .\jdbc\postgresql-42.2.9.jar;.\bin wordapp.gui.MainFrame
-rem jarファイルの作成
+rem java -cp .\jdbc\postgresql-42.2.20.jar;.\bin wordapp.gui.MainFrame
 jar cvfm wordapp.jar MANIFEST.MF -C bin .
